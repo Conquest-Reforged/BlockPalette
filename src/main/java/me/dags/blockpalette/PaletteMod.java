8f5417a5166b5852e0690a3671162f3022b66e76
dags <dags-@users.noreply.github.com>
2017-02-18 11:54:03 +0000

    Update PaletteMod.java

--- src/main/java/me/dags/blockpalette/PaletteMod.java
+++ src/main/java/me/dags/blockpalette/PaletteMod.java
@@ -14,7 +14,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class PaletteMod {
 
     public static final String MOD_ID = "blockpalette";
-    public static final String VERSION = "1.0-DEMO";
+    public static final String VERSION = "1.1";
 
     private final PaletteMain main = new PaletteMain();
     private final GameEvents events = new GameEvents(main);
