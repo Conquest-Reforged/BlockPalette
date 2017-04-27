d7adddec7da6b86935dedf4381bc1027ced417d6
dags <dags-@users.noreply.github.com>
2017-04-27 21:45:59 +0100

    version

--- src/main/java/me/dags/blockpalette/PaletteMod.java
+++ src/main/java/me/dags/blockpalette/PaletteMod.java
@@ -17,7 +17,7 @@ import java.util.Collections;
 public class PaletteMod {
 
     public static final String MOD_ID = "blockpalette";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2.0";
 
     private final PaletteMain main = new PaletteMain();
     private final GameEvents events = new GameEvents(main);
