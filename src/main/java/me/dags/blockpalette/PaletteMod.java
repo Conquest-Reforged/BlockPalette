6ac19bbb9bb1596a3a5a581ee16917c20142df23
dags- <dags@dags.me>
2017-10-22 21:41:10 +0100

    version bump

--- src/main/java/me/dags/blockpalette/PaletteMod.java
+++ src/main/java/me/dags/blockpalette/PaletteMod.java
@@ -20,7 +20,7 @@ import java.util.Collections;
 public class PaletteMod {
 
     public static final String MOD_ID = "blockpalette";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.4.3";
 
     private final PaletteMain main = new PaletteMain();
     private final GameEvents events = new GameEvents(main);
